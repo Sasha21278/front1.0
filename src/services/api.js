d3e1051066a2355e76b602243f99c74382464d2e
Aleksandr Ddd <diakal73@osu.cz>
2025-05-07 16:57:21 +0200

    init frontend1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "http://195.113.104.72:8080/api",
     withCredentials: true,
 });
 
